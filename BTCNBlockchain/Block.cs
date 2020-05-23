d47247108ffaae5b48d6ccf33e3f7b9d5b86638a
Bearnies <longvikhang@gmail.com>
2020-05-23 07:24:56 +0700

    Add project files.

---
+++ BTCNBlockchain/Block.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTCNBlockchain
+{
+    public class Block
+    {
+        public int blockIndex { get; set; }
+        public string data { get; set; }
+        public string previousHash { get; set; }
+        public string hash { get; set; }
+        public DateTime timeStamp { get; set; }
+
+        public Block(string blockPreviousHash, string blockData, DateTime blockTimeStamp)
+        {
+            blockIndex = 0;
+            previousHash = blockPreviousHash;
+            data = blockData;
+            timeStamp = blockTimeStamp;
+            hash = CalculateHash();
+        }
+
+        public string CalculateHash()
+        {
+            SHA256 sha256 = SHA256.Create();
+
+            byte[] inputBytes = Encoding.ASCII.GetBytes($"{timeStamp} - {previousHash ?? ""}-{data}"); //Dựa vào timeStamp và kiểm tra xem có previousHash không để tạo inputcode cho Block
+            byte[] outputBytes = sha256.ComputeHash(inputBytes); //Tính toán giá trị hash cho byte array inputcode
+
+            return Convert.ToBase64String(outputBytes); //Chuyển giá trị hash thành string 64-digit và return giá trị hash
+        }
+    }
+}
---
+++ BTCNBlockchain/BlockChain.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTCNBlockchain
+{
+    public class BlockChain
+    {
+        public IList<Block> blockChain { get; set; } //Các Blockchain là 1 dãy các đối tượng có thể được quản lý bằng index
+
+        public BlockChain()
+        {
+            blockChain = new List<Block>(); //Blockchain gồm nhiều block
+            blockChain.Add(CreateBlock());
+        }
+
+        public Block CreateBlock()
+        {
+            return new Block(null, "{}",DateTime.Now); //Block có thể trống
+        }
+
+        public Block GetLatestBlock() //Tìm block mới nhất
+        {
+            return blockChain[blockChain.Count - 1];
+        }
+
+        public void AddBlockToBlockChain(Block block)
+        {
+            Block latestBlock = GetLatestBlock();
+            block.blockIndex = latestBlock.blockIndex + 1;
+            block.previousHash = latestBlock.hash;
+            block.hash = block.CalculateHash();
+            blockChain.Add(block);
+        }
+    }
+}

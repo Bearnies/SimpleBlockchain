1ff2b901c1b1375f9f47f0ca6f44d55796e535ae
Bearnies <longvikhang@gmail.com>
2020-06-08 12:09:20 +0700

    Updated packages

--- BTCNBlockchain/MainWindow.xaml.cs
+++ BTCNBlockchain/MainWindow.xaml.cs
@@ -14,7 +14,6 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Base58Check;
-using NBitcoin;
 using Secp256k1Net;
 
 namespace BTCNBlockchain
